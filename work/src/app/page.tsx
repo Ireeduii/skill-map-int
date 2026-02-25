cdc74f7b4bc9f6bf70dc5c5302eb01fed96e1266
Ireeduii <25LP4866@nest.edu.mn>
2026-02-25 15:26:55 +0800

    added emp-header

---
+++ work/src/app/page.tsx
@@ -0,0 +1,9 @@
+// import { EmployeeHeader } from "./components/employee/employee-header";
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <EmployeeHeader name="iredui" />
+//     </div>
+//   );
+// }
